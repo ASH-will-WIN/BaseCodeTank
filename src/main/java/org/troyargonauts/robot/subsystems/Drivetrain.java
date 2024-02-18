5bab6926086b1c1e89bd72ff12ac054c7bc06a62
ASH-will-WIN <ashwinshrivastav4@gmail.com>
2024-02-18 18:05:17 -0500

    Changed all talons to neos
    Changed all talons to neos on this branch

--- src/main/java/org/troyargonauts/robot/subsystems/Drivetrain.java
+++ src/main/java/org/troyargonauts/robot/subsystems/Drivetrain.java
@@ -3,34 +3,37 @@ package org.troyargonauts.robot.subsystems;
 import com.ctre.phoenix6.controls.StaticBrake;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.revrobotics.CANSparkBase;
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drivetrain extends SubsystemBase {
-    private TalonFX m_FL_Motor, m_FR_Motor, m_BL_Motor, m_BR_Motor;
+    private CANSparkMax m_FL_Motor, m_FR_Motor, m_BL_Motor, m_BR_Motor;
     private double rightEncoderValue, leftEncoderValue;
     public Drivetrain(){
-        m_FL_Motor = new TalonFX(4);
-        m_FR_Motor = new TalonFX(3);
-        m_BL_Motor = new TalonFX(2);
-        m_BR_Motor = new TalonFX(0);
+        m_FL_Motor = new CANSparkMax(1, CANSparkMax.MotorType.kBrushless);
+        m_FR_Motor = new CANSparkMax(2, CANSparkMax.MotorType.kBrushless);
+        m_BL_Motor = new CANSparkMax(3, CANSparkMax.MotorType.kBrushless);
+        m_BR_Motor = new CANSparkMax(4, CANSparkMax.MotorType.kBrushless);
 
-        m_FL_Motor.setNeutralMode(NeutralModeValue.Brake);
-        m_FR_Motor.setNeutralMode(NeutralModeValue.Brake);
-        m_BL_Motor.setNeutralMode(NeutralModeValue.Brake);
-        m_BL_Motor.setNeutralMode(NeutralModeValue.Brake);
+        m_FL_Motor.setIdleMode(CANSparkBase.IdleMode.kBrake);
+        m_FR_Motor.setIdleMode(CANSparkBase.IdleMode.kBrake);
+        m_BL_Motor.setIdleMode(CANSparkBase.IdleMode.kBrake);
+        m_BR_Motor.setIdleMode(CANSparkBase.IdleMode.kBrake);
     }
 
     public void resetEndcoders(){
-        m_FL_Motor.setPosition(0);
-        m_BL_Motor.setPosition(0);
-        m_FR_Motor.setPosition(0);
-        m_BL_Motor.setPosition(0);
+        m_FL_Motor.getEncoder().setPosition(0);
+        m_BL_Motor.getEncoder().setPosition(0);
+        m_FR_Motor.getEncoder().setPosition(0);
+        m_BL_Motor.getEncoder().setPosition(0);
     }
 
     @Override
     public void periodic(){
-        rightEncoderValue = (m_FL_Motor.getPosition().getValueAsDouble() + m_BL_Motor.getPosition().getValueAsDouble())/2;
-        leftEncoderValue = (m_FR_Motor.getPosition().getValueAsDouble() + m_BR_Motor.getPosition().getValueAsDouble())/2;
+        rightEncoderValue = (m_FL_Motor.getEncoder().getPosition() + m_BL_Motor.getEncoder().getPosition())/2;
+        leftEncoderValue = (m_FR_Motor.getEncoder().getPosition() + m_BR_Motor.getEncoder().getPosition())/2;
     }
 
     public void cheesyDrive(double speed, double turn, double nerf){
