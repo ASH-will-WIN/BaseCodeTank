e377f47e767dfddd4a69a20e5fe2ad51a71f6b1a
ASH-will-WIN <ashwinshrivastav4@gmail.com>
2024-02-19 11:36:54 -0500

    Init commit for VictorSPX

--- src/main/java/org/troyargonauts/robot/subsystems/Drivetrain.java
+++ src/main/java/org/troyargonauts/robot/subsystems/Drivetrain.java
@@ -1,49 +1,54 @@
 package org.troyargonauts.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPXConfiguration;
 import com.ctre.phoenix6.controls.StaticBrake;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.wpilibj.motorcontrol.Victor;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drivetrain extends SubsystemBase {
-    private TalonFX m_FL_Motor, m_FR_Motor, m_BL_Motor, m_BR_Motor;
+    private VictorSPX m_FL_Motor, m_FR_Motor, m_BL_Motor, m_BR_Motor;
     private double rightEncoderValue, leftEncoderValue;
     public Drivetrain(){
-        m_FL_Motor = new TalonFX(4);
-        m_FR_Motor = new TalonFX(3);
-        m_BL_Motor = new TalonFX(2);
-        m_BR_Motor = new TalonFX(0);
+        m_FL_Motor = new VictorSPX(4);
+        m_FR_Motor = new VictorSPX(3);
+        m_BL_Motor = new VictorSPX(2);
+        m_BR_Motor = new VictorSPX(0);
 
-        m_FL_Motor.setNeutralMode(NeutralModeValue.Brake);
-        m_FR_Motor.setNeutralMode(NeutralModeValue.Brake);
-        m_BL_Motor.setNeutralMode(NeutralModeValue.Brake);
-        m_BL_Motor.setNeutralMode(NeutralModeValue.Brake);
+        m_FL_Motor.setNeutralMode(NeutralMode.Brake);
+        m_FR_Motor.setNeutralMode(NeutralMode.Brake);
+        m_BL_Motor.setNeutralMode(NeutralMode.Brake);
+        m_BR_Motor.setNeutralMode(NeutralMode.Brake);
     }
 
     public void resetEndcoders(){
-        m_FL_Motor.setPosition(0);
-        m_BL_Motor.setPosition(0);
-        m_FR_Motor.setPosition(0);
-        m_BL_Motor.setPosition(0);
+        m_FL_Motor.set(VictorSPXControlMode.Position, 0);
+        m_BL_Motor.set(VictorSPXControlMode.Position, 0);
+        m_FR_Motor.set(VictorSPXControlMode.Position, 0);
+        m_BR_Motor.set(VictorSPXControlMode.Position, 0);
     }
 
     @Override
     public void periodic(){
-        rightEncoderValue = (m_FL_Motor.getPosition().getValueAsDouble() + m_BL_Motor.getPosition().getValueAsDouble())/2;
-        leftEncoderValue = (m_FR_Motor.getPosition().getValueAsDouble() + m_BR_Motor.getPosition().getValueAsDouble())/2;
+        rightEncoderValue = (m_FL_Motor.getSelectedSensorPosition() + m_BL_Motor.getSelectedSensorPosition())/2;
+        leftEncoderValue = (m_FR_Motor.getSelectedSensorPosition() + m_BR_Motor.getSelectedSensorPosition())/2;
     }
 
     public void cheesyDrive(double speed, double turn, double nerf){
-        m_FL_Motor.set((speed + turn) * nerf);
-        m_BL_Motor.set((speed + turn) * nerf);
-        m_FR_Motor.set((speed - turn) * nerf);
-        m_BR_Motor.set((speed - turn) * nerf);
+        m_FL_Motor.set(VictorSPXControlMode.PercentOutput, (speed + turn) * nerf);
+        m_BL_Motor.set(VictorSPXControlMode.PercentOutput, (speed + turn) * nerf);
+        m_FR_Motor.set(VictorSPXControlMode.PercentOutput, (speed - turn) * nerf);
+        m_BR_Motor.set(VictorSPXControlMode.PercentOutput, (speed - turn) * nerf);
     }
 
     public void tankDrive(double leftJoyValue, double rightJoyValue, double nerf){
-        m_FL_Motor.set(leftJoyValue * nerf);
-        m_BL_Motor.set(leftJoyValue * nerf);
-        m_FR_Motor.set(rightJoyValue * nerf);
-        m_BR_Motor.set(rightJoyValue * nerf);
+        m_FL_Motor.set(VictorSPXControlMode.PercentOutput, (leftJoyValue * nerf));
+        m_BL_Motor.set(VictorSPXControlMode.PercentOutput, (leftJoyValue * nerf));
+        m_FR_Motor.set(VictorSPXControlMode.PercentOutput, (rightJoyValue * nerf));
+        m_BR_Motor.set(VictorSPXControlMode.PercentOutput, (rightJoyValue * nerf));
     }
 }
